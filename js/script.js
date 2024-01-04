58d2fd27a969f36e17e616818831d024ff3b7d89
sanjaycad01 <sanjayrbcad01@gmail.com>
2024-01-04 17:33:40 +0530

    Upload All Project File

---
+++ js/script.js
@@ -0,0 +1,44 @@
+function myprogram()
+{
+var a=10;
+var b=25;
+var c=60;
+console.log(a);
+var zipcode=620103;
+
+
+if((zipcode==620102) ){
+    console.log("you are logged in from Trichy");
+
+}
+else{
+    console.log("you are not logged from trichy");
+}
+
+console.log("added value "+(a+b+c));
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a=90);
+function Arthimetic()
+{
+    let a=parseInt(prompt("Enter A Value:"));
+    let b= parseInt(prompt("Enter B Value:"));
+    let c=parseInt(prompt("Enter c Value:"));
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log("added value +:"+(a+b+c));
+    console.log("Sub Value -:" +(a-b+c));
+    console.log("multipli value *: "+(a*b));
+    console.log("Division Value ?:"+(a/b));
+    console.log("Modulas %:"+(a%b));
+    
+}
+Arthimetic();
+
+
+}
+
+myprogram();
